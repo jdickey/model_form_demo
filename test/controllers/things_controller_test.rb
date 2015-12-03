26baf697e8495518633bbccba46a2f4f04a30f74
Jeff Dickey <jdickey@seven-sigma.com>
2015-12-04 00:41:50 +0800

    Added missing specs for `ThingsController#new` action method.
    65 tests, 136 assertions, 0 failures, 0 errors, 0 skips
    Coverage: 263 / 263 LOC (100.0%) covered
    RuboCop: 36 files inspected, no offenses detected
    Flay: Total score 42
    Flog: Total 206.5; method average 3.1; max 5.6 (ThingsController::IndexResponder#flash_message)
    Reek: 0 warnings

--- test/controllers/things_controller_test.rb
+++ test/controllers/things_controller_test.rb
@@ -23,4 +23,26 @@ describe 'ThingsController' do
       things.each { |thing| expect(thing).must_be_instance_of Thing }
     end
   end # describe 'GET :index'
+
+  describe 'GET :new' do
+    let(:seeded_records) { 20 }
+
+    before do
+      get :new
+    end
+
+    it 'responds with HTTP :OK (200)' do
+      expect(response).must_be :ok?
+    end
+
+    it 'renders the "things/new" template' do
+      must_render_template 'things/new'
+    end
+
+    it 'assigns a new Thing to the :thing variable' do
+      thing = assigns[:thing]
+      expect(thing).must_be_instance_of Thing
+      expect(thing).must_be :new?
+    end
+  end # describe 'GET :new'
 end # describe 'ThingsController'
