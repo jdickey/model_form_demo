28e4ede300b5fc90d95aefaef45cb8e92424b90a
Jeff Dickey <jdickey@seven-sigma.com>
2015-12-29 01:01:28 +0800

    New-thing view class' use of Bootstrap CSS classes corrected.
    Also:
    * `ThingsController::CreateResponder` now has logging added to leave
      blatant breadcrumb for "missing" flash messages (per Issue #11);
    * `Views::Things::New::EntryForm` tweaked to more consistently inject
      "magic" literals.
    
    141 tests, 200 assertions, 0 failures, 0 errors, 0 skips
    Coverage: 370 / 370 LOC (100.0%) covered
    RuboCop: 46 files inspected, no offenses detected
    Flay: Total score 42
    Flog: Total 297.9; method average 3.2; max 7.0 (Views::Things::New#flash_messages)
    Reek: 0 warnings

--- app/views/things/new.rb
+++ app/views/things/new.rb
@@ -5,33 +5,25 @@ class Views::Things::New < Views::Base
                 title_content: 'Add New Thing to List of All Things'
 
   include SemanticLogger::Loggable
-  include Views::Things::Shared
 
   def content
     flash_messages
     page_title_content
-    container { form_row }
+    form_widget
   end
 
   private
 
-  def container
-    div(class: 'container') { yield }
-  end
-
   def flash_messages
-    widget Flashes, flashes: flash
+    Rails.logger.debug 'Flashes', flash: flash.to_h
+    widget Views::Things::Shared::Flashes, flashes: flash
   end
 
-  def form_row
-    row { widget EntryForm, thing: thing }
+  def form_widget
+    widget EntryForm, thing: thing
   end
 
   def page_title_content
     content_for :title, title_content
-  end
-
-  def row
-    div(class: 'row') { yield }
   end
 end # class Views::Things::New
